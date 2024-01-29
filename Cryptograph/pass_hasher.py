4087a83bef1c228181d302c1c3bd14c0bde56789
xxeronixx <88645176+xxeronixx@users.noreply.github.com>
2024-01-29 11:51:09 -0900

    Update pass_hasher.py

--- Cryptograph/pass_hasher.py
+++ Cryptograph/pass_hasher.py
@@ -2,7 +2,6 @@ import argon2
 import qrcode
 
 def hash_password(password):
-    # Hash the password using Argon2
     hasher = argon2.PasswordHasher()
     hashed_password = hasher.hash(password)
     return hashed_password
@@ -10,7 +9,7 @@ def hash_password(password):
 def generate_qr_code(data, output_file):
     # Generate a QR code from the data
     qr = qrcode.QRCode(
-        version=1,
+        version=3, # 1-40. Changes Complexity of the QR code.
         error_correction=qrcode.constants.ERROR_CORRECT_L,
         box_size=10,
         border=4,
@@ -20,12 +19,9 @@ def generate_qr_code(data, output_file):
 
     # Create an image from the QR code
     img = qr.make_image(fill_color="black", back_color="white")
-
-    # Save the image to a file
     img.save(output_file)
 
 if __name__ == "__main__":
-    # Get password input
     password = input("Enter the text to hash: ")
 
     # Hash the password using Argon2
@@ -36,5 +32,4 @@ if __name__ == "__main__":
     qr_code_data = f"Username:{input('Username:')}"
     output_file = "hashed_password_qr.png"
     generate_qr_code(qr_code_data, output_file)
-
     print(f"QR code containing the hashed password saved to {output_file}")
